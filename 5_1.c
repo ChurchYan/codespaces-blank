5e0134988df714cdf9a2d9c0697d2f3be4db2b17
ChurchYan <hjshhb@outlook.com>
2025-07-02 02:20:31 +0000

    first commit

---
+++ 5_1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#define VAR 60
+
+int main(void) {
+    int num = 1;
+
+    
+    while(num > 0 )
+    {
+        printf("please input minutes number\n");
+        scanf("%d", &num);
+        if ( num > 0 ) {printf("Your input is %d hours and %d minutes\n", num/VAR , num%VAR);}
+        
+    }
+    
+    return 0;
+    
+}
