582d33f5db33bb2da86d8b4a5fc498fdef59bc51
ChurchYan <hjshhb@outlook.com>
2025-07-03 02:39:06 +0000

    update

--- 6/6_5.c
+++ 6/6_5.c
@@ -1,15 +1,27 @@
 #include <stdio.h>
-#define MAX_LENGTH 19
-
-int main(int argc, char const *argv[])
+int main()
 {
-    for (size_t i = 0; i < 6; i++)
+    char s = 'A';
+    int width = 24;
+    for (int i = 1; i < 10; i++)
     {
-        for (size_t k = 0; k < i; k++)
+        printf("%*s", (width - (i - 1) * 2) / 2, "");
+
+        int j = 0;
+
+        for (j; j < i - 1; j++)
         {
-            /* code */
+            printf("%c", s + j);
         }
-        
+        printf("%c", s + i - 1);
+
+        for (j; j > 0; j--)
+        {
+            printf("%c", s + j - 1);
+        }
+
+        printf("%*s", (width - (i - 1) * 2) / 2, "");
+        printf("\n");
     }
     return 0;
 }
