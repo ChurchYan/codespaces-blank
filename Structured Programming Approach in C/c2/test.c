95fab62a815fcf386dbaca67b805d06c9da2743b
ChurchYan <hjshhb@outlook.com>
2025-10-01 21:23:24 +0800

    record

---
+++ Structured Programming Approach in C/c2/test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    // printf("This line disappears.\r..A new line\n");
+    // printf("A null character\0kills the rest of the line\n");
+    // printf("\n");
+    // printf("|%-+8.2f| |%0+8.2f| |%-0+8.2f|", 1.2, 2.3, 3.4);
+
+    float x = 123.45;
+    printf("The data are: %d\n", x);
+
+    return 0;
+}
