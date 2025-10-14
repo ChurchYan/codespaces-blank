34fe7e1829f53117db113e9544a127338355f4a1
ChurchYan <hjshhb@outlook.com>
2025-10-14 09:42:09 +0000

    go home

--- Structured Programming Approach in C/c3/28.c
+++ Structured Programming Approach in C/c3/28.c
@@ -1,1 +1,16 @@
 // 28. Write a program that extracts and prints the rightmost digit of the integral portion of a float.
+
+#include <stdio.h>
+
+int main(void){
+    //local statement
+    float f;
+
+    //statement
+    printf("Please Input a float number: \n");
+    scanf(" %f", &f);
+    if (f < 0){f = -f;}
+    printf("The Rightmost digit of the integral that input is %d\n", (int)f%10);
+
+    return 0;
+}
--- Structured Programming Approach in C/c3/29.c
+++ Structured Programming Approach in C/c3/29.c
@@ -1,1 +1,20 @@
 // 29. Write a program that extracts and prints the second rightmost digit of the integral portion of a float.
+
+#include <stdio.h>
+
+int main(void){
+    //local statement
+    float f;
+
+    //statement
+    printf("Please Input a float number: \n");
+    scanf(" %f", &f);
+    if (f < 0){f = -f;}
+    if(f <= 10){
+        printf("Your Input's abs must larger than 10\n");
+        return 0;
+    }
+    printf("The second Rightmost digit of the integral that input is %d\n", (int)f%100/10);
+
+    return 0;
+}
